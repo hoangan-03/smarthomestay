b636f84154f4695f5349db2f277e672f6c3ac25c
TungVip <89310549+TungVip@users.noreply.github.com>
2024-03-23 11:15:11 +0700

    finish sidebar and home page

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,11 +1,108 @@
 import React from 'react'
+import { useState, useRef } from 'react'
 import './homepage.css'
 import homestayphoto from '../../assets/icons/homestayphoto.png'
 import lightphoto from '../../assets/icons/lightphoto.png'
 import tempphoto from '../../assets/icons/tempphoto.png'
 import detectionphoto from '../../assets/icons/detectionphoto.png'
 import setting from '../../assets/icons/setting.png'
+import leftarrow from '../../assets/icons/leftarrow.png'
+import rightarrow from '../../assets/icons/rightarrow.png'
 import { Link } from 'react-router-dom'
+
+const Images = () => {
+  const elementRef = useRef(null);
+  const [arrowDisable, setArrowDisable] = useState(true);
+
+  const handleHorizantalScroll = (element, speed, distance, step) => {
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+      element.scrollLeft += step;
+      scrollAmount += Math.abs(step);
+      if (scrollAmount >= distance) {
+        clearInterval(slideTimer);
+      }
+      if (element.scrollLeft === 0) {
+        setArrowDisable(true);
+      } else {
+        setArrowDisable(false);
+      }
+    }, speed);
+  };
+
+  return (
+    <div>
+      <div className='items-carousel'>
+        <button
+          onClick={() => {
+            handleHorizantalScroll(elementRef.current, 8, 430, -10);
+          }}
+          disabled={arrowDisable}
+          className={"absolute top-1/2 -left-9 cursor-pointer"}
+        >
+          <img src={leftarrow} alt="leftarrow" width={40} height={40}/>
+        </button>
+        <div className='home-lower-wrapper' ref={elementRef}>
+      
+        <div className="home-lower-content" >
+          <div className='bordered-content relative' >
+              <img src={tempphoto} alt="temp" className="setting-image"/>
+              <div className='home-config'>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-2xl font-bold'>Temperature</p>
+                  <p className='text-lg font-medium'>Write something here</p>
+                </div>
+                
+                <Link to="/Temperature">
+                  <img src={setting} alt="setting" className='home-setting-icon'/>
+                </Link>
+                
+              </div>
+            </div>
+
+            <div className='bordered-content relative'>
+              <img src={lightphoto} alt="light" className="setting-image"/>
+              <div className='home-config'>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-2xl font-bold'>Light Level</p>
+                  <p className='text-lg font-medium'>Write something here</p>
+                </div>
+                <Link to="/LightLevel">
+                  <img src={setting} alt="setting" className='home-setting-icon'/>
+                </Link>
+                
+              </div>
+            </div>
+
+            <div className='bordered-content relative'>
+              <img src={detectionphoto} alt="detect" className="setting-image"/>
+              <div className='home-config'>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-2xl font-bold'>Human Detection</p>
+                  <p className='text-lg font-medium'>Write something here</p>
+                </div>
+                <Link to="/HumanDetection">
+                  <img src={setting} alt="setting" className='home-setting-icon'/>
+                </Link>
+                
+              </div>
+            </div>
+        </div>
+      </div>
+        <button
+          onClick={() => {
+            handleHorizantalScroll(elementRef.current, 8, 430, 10);
+          }}
+          className={"absolute top-1/2 -right-9 cursor-pointer"}
+        >
+          <img src={rightarrow} alt="rightarr" width={40} height={40}/>
+        </button>
+      </div>
+      
+      
+    </div>
+  );
+};
 const Homepage = () => {
   return (
     <div className='home'>
@@ -17,7 +114,7 @@ const Homepage = () => {
         </div>
         
       </div>
-      <div className='home-lower-wrapper'>
+      {/* <div className='home-lower-wrapper'>
       <div className='home-lower-content'>
           <div className='bordered-content relative' >
             <img src={tempphoto} alt="temp" className="setting-image"/>
@@ -62,8 +159,8 @@ const Homepage = () => {
             </div>
           </div>
         </div>
-      </div>
-      
+      </div> */}
+      <Images />
       
       
     </div>

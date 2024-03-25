222eec0da98f4a274b76b0677f3b13c2e95ca5f2
John Nguyen <john12052003@gmail.com>
2024-03-25 21:34:51 +0700

    .

---
+++ src/components/Constant/index.js
@@ -0,0 +1,11 @@
+const data = {
+    temperature: {
+        text: "Temperature",
+        mockFigure: "31 oC"
+    },
+    lightlevel: {
+        text: "Light Level",
+        mockFigure: "70/100"
+    }
+};
+export default data;

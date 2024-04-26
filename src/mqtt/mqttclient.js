0cb6628f0c68524015340867e7c6cc895f3ca058
John Nguyen <john12052003@gmail.com>
2024-04-26 18:09:38 +0700

    .

--- src/mqtt/mqttclient.js
+++ src/mqtt/mqttclient.js
@@ -1,7 +1,7 @@
 import mqtt from "mqtt";
-const AIO_USERNAME = 'quoc_huy';
-const AIO_KEY = 'aio_eofT16nlPIWHb2VPY1lfiaMqwDbV';
-const brokerUrl = "mqtt://io.adafruit.com";
+const AIO_USERNAME = process.env.ADAIO_USERNAME;
+const brokerUrl = process.env.BROKER_URL;
+const AIO_KEY = process.env.ADAIO_KEY;
 const client = mqtt.connect(brokerUrl, {
   username: AIO_USERNAME,
   password: AIO_KEY

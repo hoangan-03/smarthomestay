1d3c59fed1a9712b5ed8c0c1e74332121d365e7b
John Nguyen <john12052003@gmail.com>
2024-04-30 14:10:04 +0700

    .

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import modeicon from '../../assets/icons/modeicon.png';
-import avataricon from '../../assets/icons/avataricon.png';
+// import avataricon from '../../assets/icons/avataricon.png';
 import darkmode from '../../assets/icons/darkmode.png';
 import { useData } from '../DataProvider';
 import Notification from '../Notification';
@@ -17,7 +17,7 @@ const Header = ({setToggleDarkMode}) => {
             {user && <Notification/>}
             <img className='cursor-pointer' src={!toggleDarkMode ? modeicon : darkmode} alt="mode" width={18} height={18} onClick={() => setToggleDarkMode(prev => !prev)}/>
             
-            <img className='cursor-pointer' src={avataricon} alt="avatar" width={45} height={45}/>
+        
         </div>
     )
 }
